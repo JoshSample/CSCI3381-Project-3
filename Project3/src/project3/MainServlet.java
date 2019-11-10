d69e2161ad671e9a190802accff564795992cf84
JoshSample <joshsample98@gmail.com>
2019-11-10 12:57:14 -0600

    Login completed

--- Project3/src/project3/MainServlet.java
+++ Project3/src/project3/MainServlet.java
@@ -1,6 +1,13 @@
 package project3;
+/*
+ * Josh Sample
+ * CSCI 3381 Project 3
+ * 11/18
+ */
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,7 +36,27 @@ public class MainServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		// This if checks login info
+		if (request.getParameter("doLogin") != null) {
+			RequestDispatcher rd;
+			String name = request.getParameter("username"); 
+			String password = request.getParameter("psw");
+			// This checks the username and password
+			// In reality this should probably be a hashmap or database type thing
+			// For simplicity, I'm leaving it hardcoded as md and pw
+			if (name.equals("md") && password.equals("pw")) {
+				rd = request.getRequestDispatcher("/using.html");
+				rd.forward(request, response); 
+			}
+			else {
+				rd = request.getRequestDispatcher("/index.html");
+				rd.forward(request, response);
+			}
+		}
+		else {
+			RequestDispatcher rd = request.getRequestDispatcher("/index.html");   
+			rd.forward(request, response);
+		}
 	}
 
 	/**
